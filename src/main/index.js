23b0c3a54a3d964ddb0b2ca40fd70782884e1147
Huailei Wang <Huailei.wang@icloud.com>
2018-09-05 14:53:39 +0800

    [done] remove console.log

--- src/main/index.js
+++ src/main/index.js
@@ -1,7 +1,6 @@
 import debug from 'electron-debug'
 import { app, globalShortcut } from 'electron'
 import ShortcutCapture from './shortcut-capture'
-console.log(require('electron'))
 debug({ showDevTools: 'undocked' })
 
 app.on('ready', () => {
@@ -9,11 +8,10 @@ app.on('ready', () => {
   installExtension.default(installExtension.VUEJS_DEVTOOLS)
     .then(() => { })
     .catch(err => {
-      console.log('Unable to install `vue-devtools`: \n', err)
+      console.error('Unable to install `vue-devtools`: \n', err)
     })
   const sc = new ShortcutCapture()
   globalShortcut.register('ctrl+alt+a', () => sc.shortcutCapture())
-  console.log(sc)
 })
 
 app.on('window-all-closed', () => {
